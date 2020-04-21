66b2ca5c1096320cdfc62958755209626767f41a
Matheus Monteiro <silvamm.matheus@gmail.com>
2020-04-20 22:07:48 -0300

    Iniciando projeto em NodeJS

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const app = express()
+const path = require('path')
+
+const port = 3000
+
+app.listen(port, () => console.log(`Funcionando | Porta : ${port}`))
+
+//config handler-bars
+app.set('views', path.join(__dirname , 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs',
+     hbs({
+        defaultLayout: 'main',
+        helpers: {
+            section: function (name, options) {
+                if (!this.section) this.section = {}
+                this.section[name] = options.fn(this)
+                return;
+            }
+        },
+        extname: 'hbs'
+    })
+)
+
+//pasta de arquivos estaticos
+app.use(express.static(path.join(__dirname , 'public')))
+app.use(express.static(path.join(__dirname , 'views')))
+
+//rotas
+app.get('/', (req, res) => res.redirect('/login'))
+app.use('/login', require('./routes/login'))
+app.use('/principal', require('./routes/principal'))
+app.use('/usuario', require('./routes/usuario'))
---
+++ views/usuario/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+    $.ajax({
+        type: 'get',
+        url: 'https://softrec.com.br/setor/',
+        contentType: 'application/json',
+        dataType: 'json'
+    }).then(function (data) {
+        console.log(data)
+        var html = ''
+        $.each(data, function (index, value) {
+            html += `<option value=${value.id}>${value.nome}</option>`
+        })
+        document.getElementById('setor').innerHTML = html
+    })
+})
+
+document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    var nome = document.getElementById('nome').value.trim()
+    var email = document.getElementById('e-mail').value.trim()
+    var senha = document.getElementById('senha').value
+    var tipo = document.getElementById('tipo').value
+    var setor = {
+        id: document.getElementById('setor').value
+    }
+
+    if (nome == '' || email == '' || senha == '') {
+        document.getElementById('resposta').innerHTML = '<strong style="color: red;">Preenchar todos os campos</strong>'
+        return
+    }
+
+    if (!validacaoEmail(email)) {
+        document.getElementById('resposta').innerHTML = '<strong style="color: red;">O e-mail é inválido</strong>'
+        return
+    }
+
+        $.ajax({
+            type: 'post',
+            url: 'https://softrec.com.br/usuario/',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                nome: nome,
+                email: email,
+                senha: senha,
+                tipo: tipo,
+                setor: setor
+            }),
+            dataType: 'json'
+        }).then(function (data) {
+            document.getElementById('resposta').innerHTML = '<strong style="color: green;">Cadastro realizado com sucesso!</strong>'
+            document.getElementById('nome').value = ''
+            document.getElementById('e-mail').value = ''
+            document.getElementById('senha').value = ''
+        }).always(function (jXHR) {
+            if (jXHR != undefined)
+                if (jXHR.status == 409)
+                    document.getElementById('resposta').innerHTML = '<strong style="color: red;">O e-mail já esta cadastrado</strong>'
+        })
+
+
+})
+
+function validacaoEmail(field) {
+    usuario = field.substring(0, field.indexOf("@"));
+    dominio = field.substring(field.indexOf("@") + 1, field.length);
+
+    if ((usuario.length >= 1) &&
+        (dominio.length >= 3) &&
+        (usuario.search("@") == -1) &&
+        (dominio.search("@") == -1) &&
+        (usuario.search(" ") == -1) &&
+        (dominio.search(" ") == -1) &&
+        (dominio.search(".") != -1) &&
+        (dominio.indexOf(".") >= 1) &&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+       return true
+    }
+    else {
+        return false
+    }
+}
+
