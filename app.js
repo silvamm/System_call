b27564a8ef8a111685dab2e13406b7ddac57fd3b
Matheus Monteiro <silvamm.matheus@gmail.com>
2020-04-26 18:59:15 -0300

    Login e logout com redis. Melhorias no Login

--- app.js
+++ app.js
@@ -2,22 +2,48 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const app = express()
 const path = require('path')
+const session = require('express-session')
+const redis = require('redis')
 
 const port = 3000
-
 app.listen(port, () => console.log(`Funcionando | Porta : ${port}`))
 
+//config recebendo dados do cliente
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
+app.use(express.json())
+
+//sessao
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient()
+
+redisClient.on('error', err => console.log('Redis error: ', err))
+
+app.use(
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: 'alpha dog',
+        resave: false,
+        saveUninitialized: true
+    })
+)
+
+//autenticacao
+global.auth = () => {
+    return function(req, res, next) {
+        if (!req.session || !req.session.email)
+            res.redirect('/login')
+        next()
+    }
+}
 
 //config handler-bars
-app.set('views', path.join(__dirname , 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.engine('hbs',
-     hbs({
+    hbs({
         defaultLayout: 'main',
         helpers: {
-            section: function (name, options) {
+            section: function(name, options) {
                 if (!this.section) this.section = {}
                 this.section[name] = options.fn(this)
                 return;
@@ -27,13 +53,19 @@ app.engine('hbs',
     })
 )
 
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
 //pasta de arquivos estaticos
-app.use(express.static(path.join(__dirname , 'public')))
-app.use(express.static(path.join(__dirname , 'views')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 //rotas
 app.get('/', (req, res) => res.redirect('/login'))
 app.use('/login', require('./routes/login'))
+app.use('/logout', require('./routes/logout'))
 app.use('/principal', require('./routes/principal'))
 app.use('/usuario', require('./routes/usuario'))
 app.use('/setor', require('./routes/setor'))
--- views/login/js/script.js
+++ views/login/js/script.js
@@ -1,25 +1,38 @@
-document.getElementById('form').addEventListener('submit', function (e) {
+let form = document.getElementById('form')
+let resposta = document.getElementById('resposta')
+let btn = document.getElementById('btnSubmit')
 
-    
+form.addEventListener('submit', function(e) {
     e.preventDefault()
-     window.location = '/principal'
-    // $.ajax({
-    //     type: 'post',
-    //     contentType: 'application/json',
-    //     url: "https://softrec.com.br/login/",
-    //     data: JSON.stringify({
-    //         email: document.getElementById('login').value,
-    //         senha: document.getElementById('password').value
-    //     }),
-    //     dataType: 'json',
-    // }).then(function (data) {
-    //     document.getElementById('resposta').innerHTML = ''
-    //     window.location = '/principal'
-    // }).always(function (jXHR) {
-    //     if (jXHR != undefined) {
-    //         if (jXHR.status = 404) {
-    //             document.getElementById('resposta').innerHTML = '<strong style="color:red;">E-mail ou senha incorretos</strong>'
-    //         }
-    //     }
-    // });
+
+    let email = document.getElementById('login').value.trim(),
+        senha = document.getElementById('password').value.trim()
+
+    if (email.length == 0 || senha.length == 0) {
+        return resposta.innerHTML = '<strong style="color:red;">Informe e-mail e senha</strong>'
+    }
+
+    resposta.innerHTML = '<strong style="color:green;">Iniciando autenticação ... </strong>'
+    btn.disabled = true
+
+    $.ajax({
+        type: 'post',
+        contentType: 'application/json',
+        url: "/login/",
+        data: JSON.stringify({
+                email: email,
+                senha: senha
+            })
+            // dataType: 'json',
+    }).done(function() {
+        resposta.innerHTML = ''
+        window.location = '/principal'
+    }).fail(function(jXHR) {
+        console.log(jXHR)
+        if (jXHR != undefined) {
+            if (jXHR.status = 404) {
+                resposta.innerHTML = '<strong style="color:red;">E-mail ou senha incorretos</strong>'
+            }
+        }
+    }).always(() => btn.disabled = false);
 })
